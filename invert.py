edf7b1b55b01071556c5f3a5ebcf0de16d85b3fa
Wangyifei <747867532@qq.com>
2019-05-31 17:19:28 +0900

    Add files via upload
    inverse kinematics of a 3R plain arm robot.  theta1 and theta2 with a range of error at 15%....
    WTF have I done?

---
+++ invert.py
@@ -0,0 +1,38 @@
+import numpy as np
+import math as m
+
+L = [10, 10, 10]
+
+def keyin():
+    l = float(input("length:"))
+    return l
+
+
+#a = float(input("final angle:"))
+#x = float(input("final x position:"))
+#y = float(input("final y position:"))
+
+a = 3
+x = -8.65837027
+y = 18.9188842
+
+Sx = x - L[2]*np.cos(a)
+Sy = y - L[2]*np.sin(a)
+print(Sx, Sy)
+
+S = Sx**2 + Sy**2
+
+theta2 = -2*m.atan(m.sqrt(((L[0]+L[1])**2 - S)/(S - (L[0]-L[1])**2)))
+D = L[0] + L[1]*np.cos(theta2)
+
+A = np.array([[D, -L[1]*np.sin(theta2)],
+              [L[1]*np.sin(theta2), D]])
+b = np.array([Sx, Sy]).T
+r = np.linalg.solve(A, b)
+print(r)
+theta1 = m.atan(r[1]/r[0])
+theta3 = a + theta1 + theta2
+
+print(theta1)
+print(theta2)
+print(theta3)
