8034b1d9642cfc83eea7f570a2e56ebec4b41f51
Wangyifei <747867532@qq.com>
2019-06-13 09:32:50 +0900

    Update 2R recursive.py

--- 2R recursive.py
+++ 2R recursive.py
@@ -3,22 +3,22 @@ import numpy as np
 t1 = np.pi/3
 t2 = -np.pi/3
 x = y = 10
-t = np.array([x, y])
+position = np.array([x, y])
 l1 = l2 = 10
 #x = float(input("final x position:"))
 #y = float(input("final y position:"))
-tv = np.array([t1, t2])
+calc_theta = np.array([t1, t2])
 n = 4
 while(n > 0):
-    X = l1*np.cos(tv[0])+l2*np.cos(tv[0]+tv[1])
-    Y = l1*np.sin(tv[0])+l2*np.sin(tv[0]+tv[1])
+    X = l1*np.cos(calc_theta[0])+l2*np.cos(calc_theta[0]+calc_theta[1])
+    Y = l1*np.sin(calc_theta[0])+l2*np.sin(calc_theta[0]+calc_theta[1])
     T = np.array([X, Y])
-    J = np.array([[-l1*np.sin(tv[0])-l2*np.sin(tv[0]+tv[1]), -l2*np.sin(tv[0]+tv[1])],
-                  [l1*np.cos(tv[0])+l2*np.cos(tv[0]+tv[1]), l2*np.cos(tv[0]+tv[1])]
+    J = np.array([[-l1*np.sin(calc_theta[0])-l2*np.sin(calc_theta[0]+calc_theta[1]), -l2*np.sin(calc_theta[0]+calc_theta[1])],
+                  [l1*np.cos(calc_theta[0])+l2*np.cos(calc_theta[0]+calc_theta[1]), l2*np.cos(calc_theta[0]+calc_theta[1])]
                   ])
     Ji = np.linalg.inv(J)
-    Terr = t - T
-    print(Terr)
-    tv = tv + Ji.dot(Terr.T)
+    Terr = position - T
+    calc_theta = calc_theta + Ji.dot(Terr.T)
     n = n-1
-print(tv)
+print(calc_theta)
+
